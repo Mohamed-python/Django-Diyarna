4fb0fdfca3f2fa15715002bc0442ec4dd74bff4e
Mohamed Gamal <mgzpython@gmail.com>
2026-01-27 14:48:50 +0200

    Donor name

--- checkout/admin.py
+++ checkout/admin.py
@@ -8,6 +8,14 @@ class OrderAdmin(admin.ModelAdmin):
     # search_fields = ('merchant_order_id', 'email', 'phone')
     # ordering = ('-created_at',)  # ترتيب من الأحدث للأقدم
     # readonly_fields = ('created_at',)  # منع تعديل تاريخ الإنشاء
-    exclude = ('donor_name', 'amount' ,'payment_status', 'cart_data')
+    exclude = ('amount' ,'payment_status', 'cart_data')
     # fields = ('email','amount')
-    readonly_fields = ('amount', 'created_at','payment_status', 'phone', 'email', 'merchant_order_id')
+    readonly_fields = (
+        'donor_name', 
+        'amount', 
+        'created_at',
+        'payment_status', 
+        'phone', 
+        'email', 
+        'merchant_order_id'
+    )
